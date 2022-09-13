a967b6a2cb14c0f0ee0d4012a8e100fe7e236b45
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-09-13 08:53:10 +0700

    chinh sua lan 1

--- chisocannang.js
+++ chisocannang.js
@@ -3,11 +3,11 @@ function clickButton(){
     let height = document.getElementById('height').value;
     let bmi = weight / Math.pow(height, 2);
     let result = null;
-     if (bmi < 18){
+     if (bmi < 18.5){
          result = 'Underweight';
-     }else if (bmi < 25){
+     }else if (bmi < 25.0){
          result = 'Normal';
-     }else if (bmi < 30){
+     }else if (bmi < 30.0){
          result = 'Overweight';
      }else{
          result = 'Obese';
